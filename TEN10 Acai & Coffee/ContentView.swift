5a6493e93ce7e17cdfa016372451683674941449
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2024-05-27 15:49:20 +0100

    Initial Commit

---
+++ TEN10 Acai & Coffee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TEN10 Acai & Coffee
+//
+//  Created by Kriti Agarwal on 27/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
